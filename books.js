934dd9ce0192cdb7bd22271d6680f3578771b0d6
Maimoona Abid <maimoonaabid2000@gmail.com>
2023-04-05 23:40:34 +0500

    db linked changed

---
+++ books.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const BooksModel = new mongoose.Schema({
+  BookName: { type: String, required: true },
+  Author: { type: String, required: true },
+  Rating: { type: String, required: true },
+  Price: { type: String, required: true },
+  ISBN: { type: String, required: true },
+  Category: { type: String, required: true },
+  CoverImage: { type: String },
+});
+
+const Book = mongoose.model("books", BooksModel);
+
+module.exports = Book;
